57966000c9233456fe3c044e03f16c8f6f3ee78f
woodi <woodi.daily@gmail.com>
2021-11-14 00:44:56 +0900

    Learn Basic Grammar

--- main.go
+++ main.go
@@ -1,10 +1,40 @@
 package main
 
 import (
-	"golwee/whale"
+	"fmt"
 )
+
+func multiply(a, b int) int{
+	return a * b
+}
+
+func superAdd(numbers ...int) int {
+	total :=0
+	for _, num := range numbers{
+		total += num
+	}
+	return total
+}
+
+func canIPlay(age int) bool {
+	switch koreanAge:= age +2; koreanAge{
+	case 10:
+		return false
+	case 18:
+		return true
+	}
+	return false
+}
+
+func repeatInfinitely(words ...string) {
+	fmt.Println(words)
+}
+
 //Entry Point
 //fmt=formatting
+//auto type inference works inside func(:=)
 func main() {
-	whale.SayGoodbye()
+	total := superAdd(1,2,3,4,5,6)
+	println(total)
+	fmt.Println(canIPlay(18))
 }
