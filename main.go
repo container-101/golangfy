63be04f7ec8f03828eceba3856397849f4e92403
woodi <woodi.daily@gmail.com>
2021-11-14 01:27:49 +0900

    Learn Go Theories

--- main.go
+++ main.go
@@ -1,41 +1,20 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-func multiply(a, b int) int{
-	return a * b
-}
-
-func superAdd(numbers ...int) int {
-	total :=0
-	for _, num := range numbers{
-		total += num
-	}
-	return total
-}
-
-func canIPlay(age int) bool {
-	switch koreanAge:= age +2; koreanAge{
-	case 10:
-		return false
-	case 18:
-		return true
-	}
-	return false
-}
-
-func repeatInfinitely(words ...string) {
-	fmt.Println(words)
+type person struct {
+	name string
+	age int
+	favFood []string
 }
 
 //Entry Point
 //fmt=formatting
 //auto type inference works inside func(:=)
 func main() {
-	a := 2
-	b := &a
-	*b = 10
-	fmt.Println(*b, a)
+	favFood := []string{"ramen"}
+	woodi :=person{
+		name:"woodi",age:18,favFood: favFood,
+	}
+	fmt.Println(woodi.name)
 }
