ccb869747e2fe2c3c071cc4d2cfcea1bad1d4f09
woodi <woodi.daily@gmail.com>
2021-11-14 03:10:59 +0900

    Complete Making Banking Module

--- banking/banking.go
+++ banking/banking.go
@@ -1,33 +1,52 @@
 package banking
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+)
 
-type BankAccount struct{
-	owner string
+type BankAccount struct {
+	owner   string
 	balance int
 }
 
-// Create New BankAccount
-func CreateBankAccount(owner string) *BankAccount{
+// CreateBankAccount Create New BankAccount
+func CreateBankAccount(owner string) *BankAccount {
 	account := BankAccount{owner: owner, balance: 0}
 	return &account
 }
+
 // Deposit x amount on BankAccount
 // Modify Actual Receiver
-func (a *BankAccount) Deposit(amount int){
+func (a *BankAccount) Deposit(amount int) {
 	a.balance += amount
 }
+
 // Balance of BankAccount
-func (a BankAccount) Balance() int{
+func (a BankAccount) Balance() int {
 	return a.balance
 }
 
 // Withdraw Balance
-func(a *BankAccount) Withdraw(amount int) error{
+func (a *BankAccount) Withdraw(amount int) error {
 	result := a.balance - amount
 	if result < 0 {
 		return errors.New("Can't withdraw(Less than Balance)")
 	}
 	a.balance = result
 	return nil
+}
+
+// ChangeOwner of the account
+func (a *BankAccount) ChangeOwner(newOwner string) {
+	a.owner = newOwner
+}
+
+// Owner of BankAccount
+func (a BankAccount) Owner() string {
+	return a.owner
+}
+
+func (a BankAccount) String() string {
+	return fmt.Sprint(a.Owner(), "'s account.\nHas: ", a.Balance())
 }
--- main.go
+++ main.go
@@ -5,15 +5,16 @@ import (
 	"golwee/banking"
 )
 
-
-
 func main() {
 	account := banking.CreateBankAccount("woodi")
 	account.Deposit(10)
 	fmt.Println(account.Balance())
-	err :=account.Withdraw(20)
+	err := account.Withdraw(20)
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println(account.Balance())
+	fmt.Println(account)
+	account.ChangeOwner("mircat")
+	fmt.Println(account)
+
 }
